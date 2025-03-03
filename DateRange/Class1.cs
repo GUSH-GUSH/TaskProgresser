c96bfa187bb9e2ec79ddb05abb7125eaaa6b7d42
gush <artemij.dp@gmail.com>
2025-03-03 22:47:10 +0200

    The start of DateRange library

---
+++ DateRange/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateRange
+{
+    public class DateRange
+    {
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+
+        public DateRange (DateTime start, DateTime end) => SetDateRange(start, end);
+
+        public void SetDateRange(DateTime start, DateTime end) {
+            if (start > end)
+                throw new ArgumentException("The argument \"start\" can't be greater than \"end\"");
+
+            this.Start = start;
+            this.End = end;
+        }
+
+    }
+}
---
+++ Weekinator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Weekinator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Weekinator")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("33945848-4662-46b6-a187-a09373bbf7bb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
