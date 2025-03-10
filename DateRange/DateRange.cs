c1c156d08a142bc41bd1f65d9b1986d1476857c6
gush <artemij.dp@gmail.com>
2025-03-10 02:20:26 +0200

    The first test of DateTime library

---
+++ DateRange/DateRange.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyUtils
+{
+    public class DateRange
+    {
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+        public TimeSpan Length { get => End - Start; }
+
+       
+        public DateRange (DateTime start, DateTime end) => SetDateRange(start, end);
+
+
+        public void SetDateRange(DateTime start, DateTime end) {
+            if (start > end)
+                throw new ArgumentException("The argument \"start\" can't be greater than \"end\"");
+
+            this.Start = start;
+            this.End = end;
+        }
+
+        /// <summary>
+        /// Принимает временную отметку, и возвращает долю, которую она составляет от текущего диапазона
+        /// </summary>
+        /// <param name="point">Временная отметка DateTime</param>
+        /// <returns>[0 ... 1]</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public double GetFractionOf(DateTime point) {
+            if (point < Start || End < point)
+                throw new ArgumentOutOfRangeException($"The point ({point}) is not in range {this.ToString()}");
+            
+            TimeSpan currentLength = point - Start;
+            double fraction = currentLength.TotalMinutes / Length.TotalMinutes;
+            
+            return fraction;
+        }
+
+
+        public override string ToString() => $"[{Start}, {End}]";
+
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyUtils;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateRange myFirstDateRange = new DateRange(new DateTime(2025,02,03), new DateTime(2025,05,23));
+
+            Console.WriteLine($"Range: {myFirstDateRange}");
+            Console.WriteLine($"Fraction: {myFirstDateRange.GetFractionOf(DateTime.Now)}");
+            Console.WriteLine($"Total precent: {Math.Round(myFirstDateRange.GetFractionOf(DateTime.Now)*100, 3)}%");
+
+            var tspn = new TimeSpan(500,60, 15, 5);
+
+            Console.WriteLine();
+            Console.WriteLine(tspn);
+            Console.WriteLine(DateTime.Now + tspn);
+
+        }
+    }
+}
