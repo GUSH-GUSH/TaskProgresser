11402ddd80c8de66b9da7e7818711ca35412477f
gush <artemij.dp@gmail.com>
2025-03-30 09:23:40 +0300

    First Step to Visual Interface

--- Weekinator/Form1.Designer.cs
+++ Weekinator/Form1.Designer.cs
@@ -28,20 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainProgressBar = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PrecentLabel = new System.Windows.Forms.Label();
+            this.StartDate = new System.Windows.Forms.DateTimePicker();
+            this.EndDate = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // MainProgressBar
+            // 
+            this.MainProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainProgressBar.Location = new System.Drawing.Point(60, 40);
+            this.MainProgressBar.Maximum = 10000;
+            this.MainProgressBar.Name = "MainProgressBar";
+            this.MainProgressBar.Size = new System.Drawing.Size(461, 23);
+            this.MainProgressBar.TabIndex = 0;
+            this.MainProgressBar.Value = 6000;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.EndDate);
+            this.panel1.Controls.Add(this.StartDate);
+            this.panel1.Controls.Add(this.PrecentLabel);
+            this.panel1.Controls.Add(this.MainProgressBar);
+            this.panel1.Location = new System.Drawing.Point(40, 246);
+            this.panel1.MinimumSize = new System.Drawing.Size(430, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(581, 90);
+            this.panel1.TabIndex = 1;
+            // 
+            // PrecentLabel
+            // 
+            this.PrecentLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PrecentLabel.AutoSize = true;
+            this.PrecentLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PrecentLabel.Location = new System.Drawing.Point(65, 17);
+            this.PrecentLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.PrecentLabel.Name = "PrecentLabel";
+            this.PrecentLabel.Size = new System.Drawing.Size(41, 20);
+            this.PrecentLabel.TabIndex = 2;
+            this.PrecentLabel.Text = "59%";
+            // 
+            // StartDate
+            // 
+            this.StartDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.StartDate.Location = new System.Drawing.Point(0, 70);
+            this.StartDate.Name = "StartDate";
+            this.StartDate.Size = new System.Drawing.Size(200, 20);
+            this.StartDate.TabIndex = 4;
+            // 
+            // EndDate
+            // 
+            this.EndDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.EndDate.Location = new System.Drawing.Point(381, 70);
+            this.EndDate.Name = "EndDate";
+            this.EndDate.Size = new System.Drawing.Size(200, 20);
+            this.EndDate.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(537, 352);
+            this.ClientSize = new System.Drawing.Size(661, 376);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(526, 200);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar MainProgressBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker EndDate;
+        private System.Windows.Forms.DateTimePicker StartDate;
+        private System.Windows.Forms.Label PrecentLabel;
     }
 }
 
--- Weekinator/Form1.cs
+++ Weekinator/Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MyUtils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,34 @@ namespace Weekinator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateRange dateRange = new DateRange(new DateTime(2025, 02, 03), new DateTime(2025, 05, 23));
+            DateTime point = DateTime.Now;
+
+            Debug.WriteLine("");
+
+            Debug.WriteLine($"Range: {dateRange}");
+            Debug.WriteLine($"Date: {point}");
+            Debug.WriteLine($"Day of semester: {dateRange.GetDayOf(point)}/{dateRange.TotalDays}");
+            Debug.WriteLine($"Num of week: {dateRange.GetWeekOf(point)}/{dateRange.TotalWeeks}");
+            Debug.WriteLine($"Day of week: {point.DayOfWeek}");
+            Debug.WriteLine($"Total precent: {Math.Round(dateRange.GetFractionOf(point) * 100, 3)}%");
+            Debug.WriteLine($"Week mark: " + ((dateRange.GetWeekOf(point) % 2 == 0) ? "Znamenyk" : "Chiselnyk"));
+
+            double fract = dateRange.GetFractionOf(point);
+
+            MainProgressBar.Value = (int)(fract * MainProgressBar.Maximum);
+            StartDate.Value = dateRange.Start;
+            EndDate.Value = dateRange.End;
+            PrecentLabel.Text = $"{Math.Round(fract * 100, 2)}%";
+
+            PrecentLabel.Location = new Point(
+                (int)(MainProgressBar.Location.X - PrecentLabel.Size.Width / 2 + MainProgressBar.Size.Width * fract),
+                PrecentLabel.Location.Y);
+
         }
     }
 }
