f8408475f24573e35f69a4835b5f0f670b742a94
gush <artemij.dp@gmail.com>
2025-09-03 01:02:21 +0300

    Генерация иконки

--- Weekinator/Forms/Main.Designer.cs
+++ Weekinator/Forms/Main.Designer.cs
@@ -28,8 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dateRangeControl1 = new Weekinator.DateRangeControl();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(346, 364);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 364);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(411, 364);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // dateRangeControl1
             // 
@@ -40,23 +82,43 @@
             this.dateRangeControl1.Size = new System.Drawing.Size(482, 90);
             this.dateRangeControl1.TabIndex = 0;
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(596, 364);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(78, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(711, 428);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.dateRangeControl1);
             this.MinimumSize = new System.Drawing.Size(526, 200);
             this.Name = "Main";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private DateRangeControl dateRangeControl1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- Weekinator/Forms/Main.cs
+++ Weekinator/Forms/Main.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿using DateTimeToolKit.Models.DateRange;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Diagnostics.SymbolStore;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
-using DateTimeToolKit.Models.DateRange;
 
 
 namespace Weekinator
@@ -57,9 +59,12 @@ namespace Weekinator
                 label_update();
             };
             timer.Start();*/
+            comboBox1_initialize();
+            comboBox2_initialize();
+
+            UpdateIcon();
 
 
-            
             dateRangeControl1.SetDateRange(new DateTime(2025, 02, 03), new DateTime(2025, 06, 23));
             dateRangeControl1.UpdateValue(DateTime.Now);
             
@@ -69,11 +74,93 @@ namespace Weekinator
                 dateRangeControl1.UpdateValue(DateTime.Now);
             };
             timer.Start();
-            
+        }
+
+        private void UpdateIcon() {
+            notifyIcon1.Icon = CreateIcon(textBox1.Text, new Font((FontFamily)comboBox1.SelectedValue, (float)numericUpDown1.Value, (FontStyle)Enum.Parse(typeof(FontStyle), comboBox2.SelectedValue?.ToString() ?? "Bold")));
+        }
+
+        private Icon CreateIcon(string text, Font font) {
+
+            Bitmap bitmap = new Bitmap(32, 32, PixelFormat.Format32bppArgb);
+           
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(Color.Transparent); // Очищаем фон, делаем его прозрачным
+
+                // Настраиваем шрифт и кисть
+                //using (Font font = new Font("Segoe UI", 12, FontStyle.Bold))
+                //{
+                    using (SolidBrush brush = new SolidBrush(Color.White))
+                    {
+                        // Рисуем текст в центре
+                        SizeF size = g.MeasureString(text, font);
+                        float x = 16 - size.Width / 2;
+                        float y = 16 - size.Height / 2;
+                        g.DrawString(text, font, brush, x, y);
+                    }
+                //}
+            }
+
+            // Создаём Icon из Bitmap. Это и есть решение!
+            IntPtr hIcon = bitmap.GetHicon();
+            Icon icon = Icon.FromHandle(hIcon);
+
+            // Важно: нужно освободить ресурсы Bitmap после использования
+            bitmap.Dispose();
+
+
+
+
+            return icon;
         }
 
         private void label_update() {
             //label1.Text = DateTime.Now.ToString();
         }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateIcon();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            UpdateIcon();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateIcon();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateIcon();
+        }
+
+        private void comboBox1_initialize() {
+            comboBox1.Items.Clear();
+
+            InstalledFontCollection installedFont = new InstalledFontCollection();
+            comboBox1.DataSource = installedFont.Families;
+            comboBox1.DisplayMember = "Name";
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox2_initialize()
+        {
+            comboBox2.Items.Clear();
+
+            comboBox2.DataSource = Enum.GetNames(typeof(FontStyle));
+
+            comboBox2.SelectedIndex = 0;
+        }
+
+    
+        /*
+private JSON
+*/
     }
 }
