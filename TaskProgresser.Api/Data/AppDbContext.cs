6b4badfca95e653c692abcf688e476bbdd4f5af1
gush <artemij.dp@gmail.com>
2026-05-10 23:21:14 +0300

    Add base code to AppDBbContext class

--- TaskProgresser.Api/Data/AppDbContext.cs
+++ TaskProgresser.Api/Data/AppDbContext.cs
@@ -1,6 +1,24 @@
-﻿ namespace TaskProgresser.Api.Data
+﻿using Microsoft.EntityFrameworkCore;
+using TaskProgresser.Core.Models;
+
+namespace TaskProgresser.Api.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Эта строчка говорит: "Создай таблицу Tasks на основе класса TaskItem"
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Здесь можно настроить то, что нельзя сделать в Core проекте
+            // Например, указать, что Title обязателен (NOT NULL)
+            modelBuilder.Entity<TaskItem>().Property(t => t.Title).IsRequired();
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
