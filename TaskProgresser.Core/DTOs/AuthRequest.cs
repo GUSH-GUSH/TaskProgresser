fe515b5681904a2227382166f9821f5ae35c9523
Gush <artemij.dp@gmail.com>
2026-05-20 12:24:26 +0300

    Add BCrypt and JWT package Add DTO Models

---
+++ TaskProgresser.Core/DTOs/AuthRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskProgresser.Core.DTOs
+{
+    public class AuthRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
